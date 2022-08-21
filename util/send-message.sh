6069feab2ddea583974d137a34285d22e9f500e0
Abraham Dika <andypex2000@yahoo.fr>
2022-08-21 09:50:16 -0500

    Update Docker container files
    Update Docker container files

--- util/send-message.sh
+++ util/send-message.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PULSAR_HOME=/Users/riferrei/Work/Pulsar/apache-pulsar-2.8.1
+export PULSAR_HOME=/Users/riferrei/Work/Pulsar/apache-pulsar-2.10.1
 
 $PULSAR_HOME/bin/pulsar-client --url pulsar://localhost:6650 \
    produce --files ${PWD}/message.json pulsar-summit
--- util/sink-start.sh
+++ util/sink-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PULSAR_HOME=/Users/riferrei/Work/Pulsar/apache-pulsar-2.8.1
+export PULSAR_HOME=/Users/riferrei/Work/Pulsar/apache-pulsar-2.10.1
 
 $PULSAR_HOME/bin/pulsar-admin --admin-url http://localhost:8080 \
    sinks start --name customSinkConnector
